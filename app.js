56ba53761468529f06a930ecdce3f891e5130d6b
Matthew Bates <matthew@nimbusec.com>
2021-01-18 08:41:36 +0100

    adds bootstrap, adds newsapi

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log('testing omas garten')
+
+// get the local news
+
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
+const url = 'http://newsapi.org/v2/top-headlines?' +
+	'country=at&' +
+	'apiKey=37a11a0f5c3b46ff84712ac8f3ae0c9f'
+	fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
+	.then(response => response.text())
+	.then(contents => console.log(contents))
+	.catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+
+	
